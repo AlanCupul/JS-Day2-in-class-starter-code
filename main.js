bb8ff3b4c307c8c8e14f7cc0a69c6490674ec5fd
Alan <alancupul@utexas.edu>
2025-02-07 16:54:44 -0600

    yes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /////// Variables Section ///////
-const myName = "Clint Tuttle"
+const myName = "Alan Cupul"
 const fruits = ['apples', 'bananas', 'blueberries', 'strawberries', 'pineapples'];
 const car = [{make:"Toyota", model:"Prius", color:"Silver"},
              {make:"Chevy", model:"Bolt", color:"Blue"}, 
@@ -39,6 +39,9 @@ console.log("Example using var");
 testVar();
 console.log("Example using LET");
 testLet();
+
+console.log("Part 1 - Var is function scoped, while Let is block scoped. We'd use let over var because it helps us to prevent overwriting variables in the global scope. Let is also easier to read, making it more predictable to understand.");
+
 // When you are done with this section you can comment out the console.logs above
 
 
@@ -51,13 +54,18 @@ const result2Element = document.getElementById('result2');
 
 ////starter code////
 //update code below to using template literals (not concatenation) to pull in the variables myName, color, make, into output
-result2Element.innerHTML = "Hello my name is {yourname} and I drive a {White Honda Civic}";
+
+// result2Element.innerHTML = "Hello my name is {yourname} and I drive a {White Honda Civic}";
+result2Element.innerHTML = `Hello my name is ${myName} and I drive a ${car[2].color} ${car[2].make} ${car[2].model}`;
 
 
 //Footer Section
 const footerElement = document.getElementById("footer");
 //update code below to use template literals to pull in the myName and the current year
-footerElement.innerHTML = "&copy; Clint Tuttle 2024";
+
+// footerElement.innerHTML = "&copy; Clint Tuttle 2024";
+footerElement.innerHTML = `&copy; ${myName} ${today.getFullYear()}`;
+
 
 
 
@@ -65,16 +73,19 @@ footerElement.innerHTML = "&copy; Clint Tuttle 2024";
 
 //////// PART 3 - Ternary Operator////////
 ////section 1 - basic if else statement
-if (loggedIn) {
-    const result3Element = document.getElementById('result3');
-    result3Element.innerHTML = "Welcome back!";
-} else {
-    const result3Element = document.getElementById('result3');
-    result3Element.innerHTML = "Please log in";
-}
+
+// if (loggedIn) {
+//     const result3Element = document.getElementById('result3');
+//     result3Element.innerHTML = "Welcome back!";
+// } else {
+//     const result3Element = document.getElementById('result3');
+//     result3Element.innerHTML = "Please log in";
+// }
 
 ////Sample code.  You can uncomment this to see how simple a ternaly operator can be
 // loggedIn ? alert("welcome back") : alert("please log in");
+
+loggedIn ?  result3Element = document.getElementById('result3').innerHTML = "Welcome back!" :  result3Element = document.getElementById('result3').innerHTML = "Please log in";
 
 ////section 2 -ternary operator to update the result3Element
 
@@ -83,37 +94,50 @@ if (loggedIn) {
 
 
 //////// PART 4 - Arrow Functions////////
-function Greetings(name) {
-    return `Hello ${name}!`;
-};
+// function Greetings(name) {
+//     return `Hello ${name}!`;
+// };
 
-console.log(Greetings("Clint"));
+// console.log(Greetings("Clint"));
 
 //Make a copy of the above function and rename it GreetingsArrow() and then convert function to an arrow function here
- 
-
-
+let GreetingsArrow = (name) => `Hello ${name}!`;
+console.log(GreetingsArrow("Alan"))
 
 //////// PART 5 - Array methods////////
-function printFruit() {
+// function printFruit() {
 
-    let resultElement = document.getElementById("result5");
+//     let resultElement = document.getElementById("result5");
 
-    resultElement.textContent = "";
+//     resultElement.textContent = "";
 
-    // Loop through the fruits array
-    for (let i = 0; i < fruits.length; i++) {
+//     for (let i = 0; i < fruits.length; i++) {
  
-        // Create a list item for each fruit
-        let p = document.createElement("p");
-        p.textContent = fruits[i];
+//         // Create a list item for each fruit
+//         let p = document.createElement("p");
+//         p.textContent = fruits[i];
 
-        // Append the list item to the ordered list
-        resultElement.appendChild(p);
-    }
+//         // Append the list item to the ordered list
+//         resultElement.appendChild(p);
+//     }
 
+//  }
+ 
+ function printFruit() {
+
+  let resultElement = document.getElementById("result5");
+  resultElement.textContent = "";
+
+  fruits
+    .filter(fruit => fruit.toLowerCase().startsWith("b"))
+    .map(fruit => {
+    let p = document.createElement("p");
+    p.textContent = fruit;
+    resultElement.appendChild(p);
+  })
  }
- 
+
+
  
  function clearText() {
     let resultElement = document.getElementById("result5");
@@ -125,20 +149,27 @@ function printFruit() {
 
 
  //////// PART6 - Destructuring////////
- myCar(car[0]);
+//  myCar(car[0]);
 
- function myCar(car) {
-     var make = car.make;
-     var model = car.model;
-     var color = car.color;
+//  function myCar(car) {
+//      var make = car.make;
+//      var model = car.model;
+//      var color = car.color;
  
-     var message = `I drive a ${color} ${make} ${model}`;
+//      var message = `I drive a ${color} ${make} ${model}`;
    
-     document.getElementById("result6").textContent = message;
- }
+//      document.getElementById("result6").textContent = message;
+//  }
 
 //convert the myCar function to use destructuring
+myCar(car[0]);
 
+function myCar(car) {
+  let { make, model, color } = car;
+  let message = `I drive a ${color} ${make} ${model}`;
+
+  document.getElementById("result6").textContent = message;
+}
 
 
 
@@ -164,13 +195,15 @@ const baseProduct = {
   // Merge basePrduct and Seasonal using spread operator and add lastUpdated property with a value of '2023-12-31'
   ///UPDATE THIS CODE
   const currentProduct = { 
-
+    ...baseProduct,
+    ...holidayPromo,
+    lastUpdated: '2023-12-31'
 };
   
   //Check the result object in the console:
   console.log(currentProduct);
   //UPDATE THIS OUTPUT
-  document.getElementById("result7").textContent = `The .....`; 
+  document.getElementById("result7").textContent = `The ${currentProduct.name} are on sale for ${currentProduct.price} as of ${currentProduct.lastUpdated}`;
 
 
 
@@ -184,12 +217,26 @@ const baseProduct = {
     let x = document.getElementById("part8-input").value;
     
     try { 
-  
+      if (x == "") {
+        throw "EMPTY";
+      }
+      if (isNaN(x)) {
+        throw "NOT A NUMBER";
+      }
+      if (x > 15) {
+        throw "TOO HIGH";
+      }
+      if (x < 10) {
+        throw "TOO LOW";
+      }
+      message.innerHTML = "Success";
+
     }
     catch(err) {
+      message.innerHTML = `Input is ${err}`;
      } 
     finally {
-
+      console.log("Input Validation Complete");
     }
 
     
